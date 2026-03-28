77451b163e445841a31d1b1a888384ae20226b2b
Dani Acosta <daniacosta.dev@gmail.com>
2026-03-28 15:51:53 -0500

    fix: link libplacebo via build.rs instead of RUSTFLAGS

--- app/build.rs
+++ app/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    println!("cargo:rustc-link-lib=placebo");
     glib_build_tools::compile_resources(
         &["../data/resources"],
         "../data/resources/resources.gresource.xml",
